1803de40b7d3e61071d5dba920768c3e342fea9b
Connor Davis <19422@kloofhigh.co.za>
2021-07-12 11:48:34 +0200

    update server to test connector

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 let { DropZoneServer } = require('@connor-davis/dropzone-protocol');
+let { Router } = require('express');
+let router = Router();
 
 let server = new DropZoneServer({
   key: 'DropZoneConnector',
   port: process.env.PORT || 8080,
 });
+
+router.get('/', async (request, response) => {
+  return response.status(200).json({
+    id: 'dropzone-node-connector',
+    message: 'Welcome to the DropZone Node Connector',
+  });
+});
+
+server.expressApp.use(router);
 
 server.listen();
